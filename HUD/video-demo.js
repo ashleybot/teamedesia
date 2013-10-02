91c4a13460a0abbe028280a3ed92b684a8789eb6
sheilpeel <schrist1@andrew.cmu.edu>
2013-10-02 16:36:37 +0100

    First commit

---
+++ HUD/video-demo.js
@@ -0,0 +1,8 @@
+var menuImage = document.getElementById('foodImage');
+
+function leftClicked () {
+	menuImage.setAttribute("src","fish_dish.png");
+}
+function rightClicked() {
+	menuImage.setAttribute("src","dessert_dish.png");
+}
